a332670bb2d692251a39208f3889eb716769d574
Eximius <eximius@gmx.com>
2015-05-08 16:08:42 +0300

    Screw bytes, encode string to utf8

--- GameSession.py
+++ GameSession.py
@@ -268,7 +268,7 @@ class GameSession(QObject):
             logger.warn('Unknown file id: %d', id)
             return
 
-        file.write(b' '.join([bytes(x) for x in args])+b'\n')
+        file.write((' '.join([str(x) for x in args])+'\n').encode())
 
     def _FClose(self, id):
 
